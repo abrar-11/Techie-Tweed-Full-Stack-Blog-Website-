8084ad0497756c771ac7308ebe24c43784cc2d50
abar ahmed <abrarahmed2185@gmail.com>
2022-10-01 00:10:49 +0500

    Navbar and Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Layout from "./layouts/Layout";
 import { Home, Login, Register, Post } from "./pages/index";
 
 const router = createBrowserRouter([
    {
       path: "/",
-      element: <Home/>,
+      element: (
+         <Layout>
+            <Home />
+         </Layout>
+      ),
    },
 ]);
 
