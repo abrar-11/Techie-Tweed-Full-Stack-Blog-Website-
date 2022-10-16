6dbea6df783492a5656c1447486da624682b0f09
msaadhassan00@ <msaadhassan00@gmail.com>
2022-10-16 23:12:53 +0500

    get Blog by Id

--- server/controllers/blogControllers.js
+++ server/controllers/blogControllers.js
@@ -15,6 +15,7 @@ export const getAllBlogs = (req, res) => {
       });
    });
 };
+
 export const createBlog = (req, res) => {
    const q =
       "insert into blogs (`title`,`description`,`img`,`author`) values (?)";
@@ -38,3 +39,16 @@ export const createBlog = (req, res) => {
       });
    });
 };
+
+export const getBlog = (req, res) => {
+   const q = "SELECT `username`,`title`,`desc`,p.img, u.img AS userImg ,`cat`,`date` FROM users u JOINS posts p ON u.id = p.uid WHERE p.id = ?"
+   db.query(q, [req.params.id], (err, results) => {
+      if (err) {
+         return res.status(500).json({ success: false, error: err });
+      }
+      console.log(results[0]);
+      res.status(200).json({
+         success: true,
+      });
+   });
+}
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -7,6 +7,7 @@ const router  =  express.Router()
 
 
 router.get('/',getAllBlogs)
+router.get('/:id',getBlog)
 router.post('/',createBlog)
 
 
